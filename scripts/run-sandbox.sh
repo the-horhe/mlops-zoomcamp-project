fa597e98f6118328c03fbedf701e2beb142e5f92
aleksandr.tretyakov <aleksandr.tretyakov@semrush.com>
2024-07-22 15:48:30 +0200

    Add dagster persistant storage

--- scripts/run-sandbox.sh
+++ scripts/run-sandbox.sh
@@ -2,8 +2,8 @@
 
 set -uoe pipefail
 
+# Docker-compose
 docker-compose -f docker/docker-compose.yaml stop
-
 docker-compose -f docker/docker-compose.yaml up --build -d
 
 # TODO: print services links
