ac05c396fe4361eefdf5c2cecb7787743c69a25b
aleksandr.tretyakov <aleksandr.tretyakov@semrush.com>
2024-07-23 14:37:52 +0200

    Dockerize prediction service

--- scripts/run-sandbox.sh
+++ scripts/run-sandbox.sh
@@ -2,8 +2,24 @@
 
 set -uoe pipefail
 
+
+COMPOSE_FILES=" -f docker/docker-compose.yaml "
+while [ "${1-}" != "" ]; do
+    case $1 in
+        --with-service )
+          shift
+          COMPOSE_FILES="$COMPOSE_FILES -f docker/docker-compose-service.yaml"
+        ;;
+        --stop )
+          shift
+          docker-compose $COMPOSE_FILES stop
+          exit 0
+        ;;
+    esac
+done
+
 # Docker-compose
-docker-compose -f docker/docker-compose.yaml stop
-docker-compose -f docker/docker-compose.yaml up --build -d
+docker-compose $COMPOSE_FILES stop
+docker-compose $COMPOSE_FILES up --build -d
 
 # TODO: print services links
