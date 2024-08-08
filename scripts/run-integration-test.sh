b1016df745fdb5a89c3b15bfe4f64c67ab599b5c
aleksandr.tretyakov <aleksandr.tretyakov@semrush.com>
2024-08-08 19:46:14 +0200

    CI: fix integration tests

--- scripts/run-integration-test.sh
+++ scripts/run-integration-test.sh
@@ -15,22 +15,18 @@ poetry run python3 tests/utils/register_latest_model.py
 # Restart service to reload model
 # TODO: restart only web-service container, but with health status await
 bash scripts/run-sandbox.sh --stop
-bash scripts/run-sandbox.sh --with-service || true
 
-ls -lah
-ls -lah volumes/mlflow
+export DP_MLFLOW_MODEL_NAME="drought-prediction-test"
+bash scripts/run-sandbox.sh --with-service
 
-docker compose -f docker/docker-compose-service.yaml logs prediction-service
-
-exit 1
 
 # Get prediction
 prediction=$(curl -X POST http://127.0.0.1:8000/predict -H "Content-Type: application/json" -d '{"lag_365": [0]}')
 
 # Check results
-expected_prediction='{"predictions":[16.362569259476018]}'
+expected_prediction='{"predictions":[14.350989815313053]}'
 if [ "$prediction" != $expected_prediction ]; then
-  echo "Invalid prediction: {expected_prediction}"
+  echo "Invalid prediction: $expected_prediction"
   bash -c "scripts/run-sandbox.sh --with-service --stop"
   exit 1
 fi
