9d410e1aed46b440096c1e2e83c484c63bf74d5d
aleksandr.tretyakov <aleksandr.tretyakov@semrush.com>
2024-08-08 19:07:32 +0200

    CI: fix integration test web-service

--- scripts/run-integration-test.sh
+++ scripts/run-integration-test.sh
@@ -3,9 +3,7 @@
 set -uoe pipefail
 
 # Prepare sandbox environment
-bash scripts/run-sandbox.sh --with-service || true
-
-docker compose -f docker/docker-compose-service.yaml logs prediction-service
+bash scripts/run-sandbox.sh
 
 # Run training  pipeline
 export DP_DATA_PATH="tests/data/data_sample_integration.csv"
@@ -17,11 +15,7 @@ poetry run python3 tests/utils/register_latest_model.py
 # Restart service to reload model
 # TODO: restart only web-service container, but with health status await
 bash scripts/run-sandbox.sh --with-service --stop
-bash scripts/run-sandbox.sh --with-service || true
-
-docker compose -f docker/docker-compose-service.yaml logs prediction-service
-
-exit 1
+bash scripts/run-sandbox.sh --with-service
 
 # Get prediction
 prediction=$(curl -X POST http://127.0.0.1:8000/predict -H "Content-Type: application/json" -d '{"lag_365": [0]}')
